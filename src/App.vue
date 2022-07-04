8d71fa7656fae00e9ae365ae783bb4b611d34e62
RodgersX <brianmawira2@gmail.com>
2022-07-04 15:54:07 +0300

    successfully display gps weather data

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <v-app>
-    <v-app-bar app color="green" dark>
+    <header app flat dark class="header d-flex justify-space-between">
       <v-app-bar-title>
-        <h2>CyWeather</h2>
+        <h2 class="green--text">CyWeather</h2>
       </v-app-bar-title>
-    </v-app-bar>
+      <v-spacer />
+      <v-btn outlined color="green">About CyWeather</v-btn>
+    </header>
 
-    <v-main>
-      <section class="form-div">
+    <main class="main-app d-flex justify-space-around pa-11">
+      <section class="form-div" style="height: 70vh">
         <h2 class="mb-10">
           Enter the longitude and Latitude to get the weather
         </h2>
         <form>
-          <v-text-field label="Latitude" color="green" v-model="latitude">
+          <v-text-field
+            label="Latitude"
+            color="green"
+            v-model="latitude"
+            class="input-field"
+          >
           </v-text-field>
-          <v-text-field label="Longitude" color="green" v-model="longitude">
+          <v-text-field
+            label="Longitude"
+            color="green"
+            v-model="longitude"
+            class="input-field green--text"
+          >
           </v-text-field>
           <v-btn
             @click.prevent="getWeatherData"
@@ -28,16 +40,19 @@
         </form>
       </section>
 
-      <section class="mb-11">
-        <div class="section-title text-center mb-5">
-          <h2>Today's Weather forecast</h2>
+      <section
+        class="weather-data mb-11"
+        v-if="alignedData.length > 0"
+        style="width: 50%"
+      >
+        <div class="section-title text-center white--text mb-5">
+          <h2>Today's Weather Forecast</h2>
         </div>
-        <div class="d-flex weather-cards">
-          <v-card
-            tabindex="1"
-            color="green lighten-4"
+        <v-layout row wrap class="weather-cards">
+          <div
+            width="300px"
             class="weather-div text-center pa-1 d-flex justify-space-between"
-            v-for="(data, i) in alignedDataFromAllLists"
+            v-for="(data, i) in alignedData"
             :key="i"
           >
             <div class="shown-details">
@@ -50,34 +65,33 @@
               <p class="mt-2">{{ data.temperature }} &#8451;</p>
             </div>
 
-            <div class="separator" style="width: 100px"></div>
+            <div class="separator" style="width: 100px" />
 
-            <div class="hidden-details">
-              <v-list>
+            <div class="more-details d-flex align-center">
+              <v-list color="transparent">
                 <v-list-item>
-                  <v-list-item-content>
+                  <v-list-item-content class="white--text">
                     Windspeed: {{ data.windspeed }} km/h
                   </v-list-item-content>
                 </v-list-item>
                 <v-divider></v-divider>
                 <v-list-item>
-                  <v-list-item-content>
+                  <v-list-item-content class="white--text">
                     Humidity levels: {{ data.humidity }} %
                   </v-list-item-content>
                 </v-list-item>
                 <v-divider></v-divider>
                 <v-list-item>
-                  <v-list-item-content>
+                  <v-list-item-content class="white--text">
                     Cloud Cover: {{ data.cloudcover }} %
                   </v-list-item-content>
                 </v-list-item>
               </v-list>
             </div>
-          </v-card>
-        </div>
-        <div class="more-details"></div>
+          </div>
+        </v-layout>
       </section>
-    </v-main>
+    </main>
   </v-app>
 </template>
 
@@ -94,6 +108,7 @@ export default {
 
       hourlyWeatherData: null,
       hourlyUnits: null,
+      alignedData: [],
     };
   },
 
@@ -103,62 +118,92 @@ export default {
     },
   },
 
-  computed: {
-    alignedDataFromAllLists() {
-      return this.hourlyWeatherData.time.map((time, i) => {
-        return {
-          time: time,
-          temperature: this.hourlyWeatherData.temperature_2m[i],
-          humidity: this.hourlyWeatherData.relativehumidity_2m[i],
-          windspeed: this.hourlyWeatherData.windspeed_120m[i],
-          cloudcover: this.hourlyWeatherData.cloudcover[i],
-        };
-      });
-    },
-  },
-
-  created() {
-    const resp = this.$http
-      .get(
+  async created() {
+    try {
+      const result = await this.$http.get(
         `/v1/forecast?latitude=52.52&longitude=12.45&hourly=temperature_2m,relativehumidity_2m,cloudcover,windspeed_120m`
-      )
-      .then((result) => {
-        this.hourlyWeatherData = result.data.hourly;
-      })
-      .catch((err) => console.log(err.message));
+      );
+      this.hourlyWeatherData = result.data.hourly;
+    } catch (e) {
+      console.log(e.message);
+    }
   },
 
   methods: {
     async getWeatherData() {
-      const resp = await this.$http.get(
-        `/v1/forecast?latitude=${this.latitude}&longitude=${this.longitude}&hourly=temperature_2m,relativehumidity_2m,cloudcover,windspeed_120m`
-      );
-      // this.hourlyWeatherData = resp.data.hourly;
-      // this.hourlyUnits = resp.data.hourly_units;
+      try {
+        if (this.latitude === "" && this.longitude === "") {
+          alert("The latitude and longitude must be filled!");
+        } else {
+          const resp = await this.$http.get(
+            `/v1/forecast?latitude=${this.latitude}&longitude=${this.longitude}&hourly=temperature_2m,relativehumidity_2m,cloudcover,windspeed_120m`
+          );
+          this.hourlyWeatherData = resp.data.hourly;
+          this.hourlyUnits = resp.data.hourly_units;
+          this.groupAllData();
+        }
+      } catch (e) {
+        console.log(e.message);
+      }
+    },
+
+    groupAllData() {
+      this.hourlyWeatherData.time.map((time, i) => {
+        this.alignedData.push({
+          time: time,
+          humidity: this.hourlyWeatherData.relativehumidity_2m[i],
+          windspeed: this.hourlyWeatherData.windspeed_120m[i],
+          temperature: this.hourlyWeatherData.temperature_2m[i],
+          cloudcover: this.hourlyWeatherData.cloudcover[i],
+        });
+      });
+    },
+
+    isInRange(value, range) {
+      return value >= range[0] && value <= range[1];
     },
   },
 };
 </script>
 
 <style lang="sass" scoped>
+  .header
+    height: 70px
+    display: flex
+    justify-content: space-between
+    align-items: center
+    padding: 0 16px
+    background-color: rgba(0, 0, 0, 0.7)
+
+  .main-app
+    background-image: url("./assets/weather-bck.jpg")
+    background-size: cover
+    height: 100vh
+
 .form-div
-  width: 50%
-  margin: 4rem auto
+  width: 40%
   border: 1px solid grey
+  background-color: rgba(0, 0, 0, 0.6)
+  color: white
   padding: 3rem
   border-radius: 10px
 
 .weather-data
-  width: 95%
-  margin: 1rem auto
-  display: flex
+  overflow-y: scroll
+  overflow-x: hidden
+  -ms-overflow-style: none
+  scrollbar-width: none
 
-.weather-cards
-  overflow-x: scroll
+.weather-data::-webkit-scrollbar
+  display: none
 
-.v-card
-  margin: 0 10px
+.weather-div
+  background-color: rgba(208, 197, 203, 0.2)
+  margin: 6px
+  color: white
 
-.v-card:focus
-  width: 200px
+.input-field
+  color: white
+
+
 </style>
---
+++ src/components/weather-detail-card.vue
@@ -0,0 +1,15 @@
+<template></template>
+
+<script>
+export default {
+  data() {
+    return {
+      key: value,
+    };
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
